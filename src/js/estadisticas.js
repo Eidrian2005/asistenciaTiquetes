978a3a67ffa4587730cf5bdbc55e8b232ae10770
Eidrian2005 <eserrano@fwdcostarica.com>
2024-08-23 15:30:50 -0600

    hay que trabajar los post delete y get

--- src/js/estadisticas.js
+++ src/js/estadisticas.js
@@ -1,9 +1,10 @@
-import {postUsers} from '../../services/postUsers.js'  
-import { getCons } from "../../services/getUsers"
+import {postUsers} from '../../services/postUsers.js';
+import { getCons } from "../../services/obtenerCons.js"
 import { getUsers } from "../../services/getUsers";
 
 
-async function obtenerUsuario() {
+
+async function obtenerConsulta() {
     const usuario = await getCons()
 
     usuario.forEach(usuario => {
@@ -11,7 +12,7 @@ async function obtenerUsuario() {
     });
 }
 
-obtenerUsuario()
+obtenerConsulta()
 
 
 
